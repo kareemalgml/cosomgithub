e0f0d1d04c569a77f6c968613b33b514485f6697
kareemalgml <139414950+kareemalgml@users.noreply.github.com>
2023-07-13 07:44:11 +0300

    Add files via upload

---
+++ zby.js
@@ -0,0 +1,19 @@
+// // JavaScriptz
+// var button = document.getElementById('myButton');
+// button.addEventListener('click', function() {
+//   // Redirect the user to another page
+//   window.location.href = '/happy-birthday-master/img gallery bs-5/img gallery/index.html';
+// });
+
+// // Get the button element
+// var button = document.getElementById("myButton");
+
+// // Hide the button initially
+// button.style.display = "none";
+
+// // Show the button after 3 minutes
+// setTimeout(function() {
+//   button.style.display = "block";
+//   button.style.marginLeft = "auto";
+//   button.style.marginRight = "auto";
+// }, (3 * 60 + 20) * 1000); // 3 minutes and 20 seconds in milliseconds
